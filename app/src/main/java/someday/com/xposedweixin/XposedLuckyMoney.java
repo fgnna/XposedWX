e6cc5560b13261bdb6afbe038535449e6267b088
someday <fgnna@qq.com>
2019-03-13 16:45:42 +0800

    整理代码，修复bug

--- app/src/main/java/someday/com/xposedweixin/XposedLuckyMoney.java
+++ app/src/main/java/someday/com/xposedweixin/XposedLuckyMoney.java
@@ -5,10 +5,12 @@ import android.content.ContentValues;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
@@ -28,162 +30,193 @@ public class XposedLuckyMoney implements IXposedHookLoadPackage
 
     private String lastTalkerName = "";
     private String lastNativeUrl = "";
-    private Activity launcherUiActivity;
+    private Activity launcherUiActivity;//微信主界面
+
+    private boolean isRun = false;
+
+    private ConcurrentLinkedQueue mConcurrentLinkedQueue = new ConcurrentLinkedQueue();
+
+    private Thread mTaskRun = new Thread(){
+        @Override
+        public void run()
+        {
+            while (true)
+            {
+                Object o = mConcurrentLinkedQueue.poll();
+                synchronized(this)
+                {
+                    if(null == o)
+                    {
+                        try
+                        {
+
+                            this.wait();
+                        } catch (InterruptedException e)
+                        {
+                            e.printStackTrace();
+                        }
+                    }
+                    else
+                    {
+
+                        log("获取一个任务 ");
+
+                    }
+                }
+
+            }
+        }
+    };
 
 
     @Override
     public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable
     {
-        log("Loaded app: " + loadPackageParam.packageName);
-        Log.d("YOUR_TAG", "Loaded app: " + loadPackageParam.packageName);
+//        log("packageName:"+loadPackageParam.processName +":"+this);
+
         // Xposed模块自检测
         if (loadPackageParam.packageName.equals("someday.com.xposedweixin"))
         {
             XposedHelpers.findAndHookMethod("someday.com.xposedweixin.MainActivity", loadPackageParam.classLoader, "isModuleActive", XC_MethodReplacement.returnConstant(true));
         }
-        if (loadPackageParam.packageName.equals("com.tencent.mm"))
+
+
+
+        if (loadPackageParam.processName.equals("com.tencent.mm"))
         {
-            findAndHookMethod("com.tencent.wcdb.database.SQLiteDatabase", loadPackageParam.classLoader, "insertWithOnConflict", String.class, String.class, ContentValues.class, int.class,
-                    new XC_MethodHook()
+            if(!isRun)
             {
-                @Override
-                protected void afterHookedMethod(MethodHookParam param) throws Throwable
-                {
-                    log("table=" + param.args[0] + "\n");
-                    ContentValues contentValues = (ContentValues) param.args[2];
-
-                    if ("WalletLuckyMoney".equals(param.args[0]))
-                    {
-                        log("------------------------红包参数---------------------" + "\n");
-                        log("mNativeUrl:" + contentValues.getAsString("mNativeUrl"));
-                        lastNativeUrl = contentValues.getAsString("mNativeUrl");
-                        log("------------------------红包参数 END---------------------" + "\n");
-
-
-                    } else if ("message".equals(param.args[0]) && null != contentValues.getAsString("reserved") && contentValues.getAsString("reserved").indexOf("微信红包") != -1)
-                    {
-                        log("------------------------红包消息---------------------" + "\n");
-                        log("talker:" + contentValues.getAsString("talker"));
-                        lastTalkerName = contentValues.getAsString("talker");
-//                        log("content:"+contentValues.getAsString("content"));
-                        log("------------------------红包消息 END---------------------" + "\n");
-                        // 启动红包页面
-                        if (launcherUiActivity != null)
-                        {
-                            log("触发一个开红包的窗口" + "\n");
-                            Intent paramau = new Intent();
-                            paramau.putExtra("key_way", 0);
-                            paramau.putExtra("key_native_url", lastNativeUrl);
-                            paramau.putExtra("key_username", lastTalkerName);
-                            paramau.putExtra("key_cropname", (String) null);
-                            callStaticMethod(findClass("com.tencent.mm.br.d", loadPackageParam.classLoader), "b", launcherUiActivity, "luckymoney", ".ui.LuckyMoneyNotHookReceiveUI", paramau);
-                        } else
-                        {
-                            log("launcherUiActivity == null" + "\n");
-                        }
-                    }
-
-
-//
-//                    log("------------------------insert over---------------------" + "\n\n");
-//                    log("------------------------insertWithOnConflict called---------------------" + "\n");
-//                    log("------------------------insert start---------------------" + "\n\n");
-//                    log("param args1:" + (String)param.args[0]);
-//                    log("param args1:" + (String)param.args[1]);
-//                    ContentValues contentValues = (ContentValues) param.args[2];
-//                    log("param args3 contentValues:");
-//                    for (Map.Entry<String, Object> item : contentValues.valueSet())
-//                    {
-//                        if (item.getValue() != null)
-//                        {
-//                            log(item.getKey() + "---------" + item.getValue().toString());
-//                        }
-//                        else
-//                        {
-//                            log(item.getKey() + "---------" + "null");
-//                        }
-//                    }
-//
-//                    log("------------------------insert over---------------------" + "\n\n");
-
-
-                }
-            });
-
-//            findAndHookMethod("com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyNotHookReceiveUI", loadPackageParam.classLoader, "onCreate", Bundle.class, new XC_MethodHook() {
-//                @Override
-//                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-//                    log("LuckyMoneyNotHookReceiveUI");
-//                    Activity activity = (Activity) param.thisObject;
-//                    String key_username = activity.getIntent().getStringExtra("key_username");
-//                    String key_native_url = activity.getIntent().getStringExtra("key_native_url");
-//                    String key_cropname = activity.getIntent().getStringExtra("key_cropname");
-//                    int key_way = activity.getIntent().getIntExtra("key_way", 0);
-//                    log("key_username: " + key_username + "\n");
-//                    log("key_native_url: " + key_native_url + "\n");
-//                    log("key_cropname: " + key_cropname + "\n");
-//                    log("key_way: " + key_way + "\n");
-//
-////                    callMethod(param.thisObject,"iA",true);
-//                    Class clasz = param.thisObject.getClass();
-//                    Method[] methods = clasz.getDeclaredMethods();
-//                    for(Method method : methods)
-//                    {
-//                        log("methods name: " + method.getName());
-//                        if("iA".equals(method.getName()))
-//                        {
-//                            method.invoke(param.thisObject,true);
-//                        }
-//                    }
-//                }
-//            });
-
-            findAndHookMethod("com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyNotHookReceiveUI", loadPackageParam.classLoader, "c", int.class, int.class, String.class,
-                    loadPackageParam.classLoader.loadClass("com.tencent.mm.ah.m"), new XC_MethodHook()
-            {
-                @Override
-                protected void afterHookedMethod(MethodHookParam param) throws Throwable
-                {
-                    log("LuckyMoneyNotHookReceiveUI");
-                    Field buttonField = XposedHelpers.findField(param.thisObject.getClass(), "nai");
-                    final Button kaiButton = (Button) buttonField.get(param.thisObject);
-                    kaiButton.performClick();
-
-                }
-            });
-            findAndHookMethod("com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyDetailUI", loadPackageParam.classLoader, "onCreate", Bundle.class,new XC_MethodHook()
-            {
-                @Override
-                protected void afterHookedMethod(MethodHookParam param) throws Throwable
-                {
-                    ((Activity)param.thisObject).finish();
-                }
-            });
-//            findAndHookMethod("com.tencent.mm.ui.chatting.d.a", loadPackageParam.classLoader, "getTalkerUserName",  new XC_MethodHook() {
-//                @Override
-//                protected void afterHookedMethod(MethodHookParam param) throws Throwable
-//                {
-//                    super.afterHookedMethod(param);
-//                    log("getTalkerUserName:"+param.getResult());
-//
-//                }
-//
-//                @Override
-//                protected void beforeHookedMethod(MethodHookParam param) throws Throwable
-//                {
-//                    super.beforeHookedMethod(param);
-//                    log("getTalkerUserName:"+param.getResult());
-//                }
-//            });
-            findAndHookMethod("com.tencent.mm.ui.LauncherUI", loadPackageParam.classLoader, "onCreate", Bundle.class, new XC_MethodHook()
-            {
-                @Override
-                protected void afterHookedMethod(MethodHookParam param) throws Throwable
-                {
-                    launcherUiActivity = (Activity) param.thisObject;
-                }
-            });
+                isRun = true;
+                mTaskRun.setPriority(1);
+                mTaskRun.start();
+            }
+            initHook(loadPackageParam);//初始化动作
+            hookNewMessage(loadPackageParam);//追踪红包消息
+            hookOpenLuckyMoney(loadPackageParam);//自动点开红包
+            hookCloseLuckyMoneyDetailUI(loadPackageParam);//自动关闭红包详情页
         }
 
     }
+
+    /**
+     * 初始化微信红包勾子
+     * @param loadPackageParam
+     */
+    public void initHook(XC_LoadPackage.LoadPackageParam loadPackageParam)
+    {
+        findAndHookMethod("com.tencent.mm.ui.LauncherUI", loadPackageParam.classLoader, "onCreate", Bundle.class, new XC_MethodHook()
+        {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable
+            {
+                launcherUiActivity = (Activity) param.thisObject;
+            }
+        });
+    }
+
+
+    /**
+     * 过滤红包消息
+     *
+     * @param loadPackageParam
+     */
+    public void hookNewMessage(final XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable
+    {
+        findAndHookMethod("com.tencent.wcdb.database.SQLiteDatabase", loadPackageParam.classLoader, "insertWithOnConflict", String.class, String.class, ContentValues.class, int.class,
+                new XC_MethodHook()
+                {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable
+                    {
+                        log("table=" + param.args[0] + "\n");
+                        ContentValues contentValues = (ContentValues) param.args[2];
+
+                        if ("WalletLuckyMoney".equals(param.args[0]))
+                        {
+                            log("------------------------红包参数---------------------" + "\n");
+                            log("mNativeUrl:" + contentValues.getAsString("mNativeUrl"));
+                            lastNativeUrl = contentValues.getAsString("mNativeUrl");
+                            log("------------------------红包参数 END---------------------" + "\n");
+
+
+                        } else if ("message".equals(param.args[0]) && null != contentValues.getAsString("reserved") && contentValues.getAsString("reserved").indexOf("微信红包") != -1)
+                        {
+                            log("------------------------红包消息---------------------" + "\n");
+                            log("talker:" + contentValues.getAsString("talker"));
+                            lastTalkerName = contentValues.getAsString("talker");
+//                        log("content:"+contentValues.getAsString("content"));
+                            log("------------------------红包消息 END---------------------" + "\n");
+                            // 启动红包页面
+                            if (launcherUiActivity != null)
+                            {
+                                log("触发一个开红包的窗口" + "\n");
+                                Intent paramau = new Intent();
+                                paramau.putExtra("key_way", 0);
+                                paramau.putExtra("key_native_url", lastNativeUrl);
+                                paramau.putExtra("key_username", lastTalkerName);
+                                paramau.putExtra("key_cropname", (String) null);
+                                callStaticMethod(findClass("com.tencent.mm.br.d", loadPackageParam.classLoader), "b", launcherUiActivity, "luckymoney", ".ui.LuckyMoneyNotHookReceiveUI", paramau);
+                                mConcurrentLinkedQueue.add(new Object());
+//                                mTaskRun.notifyAll();
+                            } else
+                            {
+                                log("launcherUiActivity == null" + "\n");
+                            }
+                        }
+
+
+                    }
+                });
+    }
+
+    /**
+     * 触发开红包动作
+     */
+    public void hookOpenLuckyMoney(final XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable
+    {
+        findAndHookMethod("com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyNotHookReceiveUI", loadPackageParam.classLoader, "onResume", new XC_MethodHook()
+                {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable
+                    {
+                        log("LuckyMoneyNotHookReceiveUI");
+                        Field buttonField = XposedHelpers.findField(param.thisObject.getClass(), "nai");
+                        final Button kaiButton = (Button) buttonField.get(param.thisObject);
+                        if(kaiButton.getVisibility() != View.VISIBLE)
+                        {
+                            ((Activity)param.thisObject).finish();
+                        }
+                        else
+                        {
+                            //这里必需要延迟0.5秒点击，不然有时不生效
+                            kaiButton.postDelayed(new Runnable()
+                            {
+                                @Override
+                                public void run()
+                                {
+                                    kaiButton.performClick();
+
+                                }
+                            },500);
+                        }
+
+                    }
+                });
+    }
+
+    /**
+     * 关闭红包详情页
+     */
+    public void hookCloseLuckyMoneyDetailUI(final XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable
+    {
+        findAndHookMethod("com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyDetailUI", loadPackageParam.classLoader, "onCreate", Bundle.class, new XC_MethodHook()
+        {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable
+            {
+                ((Activity) param.thisObject).finish();
+            }
+        });
+    }
+
 }
